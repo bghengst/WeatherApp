5cc8ab1f639e6f354aa19e1cce139461848d6d71
Grant <beauhengst@gmail.com>
2020-12-16 20:38:59 -0500

    added variable and function to retrieve current weather conditions from API named variables within it

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ var ownKey = "f54881811ae9445900b1b062062864be";
 var currentCity = "";
 var lastCity = "";
 
+var anError = (response) => {
+    if(!response.ok){
+        throw Error(response.statusText);
+    }
+    return response;
+}
+
+var currentWeather = (event) => {
+    let city = $('#search-city').val();
+    currentCity = $("#search-city").val();
+
+    let 
+}
 
 
 api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
